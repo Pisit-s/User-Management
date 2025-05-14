7d7193a3c6f9fa4dceb4e739101ad34c65e63709
Pisit <pisit.sr@kkumail.com>
2025-05-15 00:41:53 +0700

    debug for deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { ButtonPages } from '../components/InputComponent'
-import logo from '../public/logoowen.svg'
 
 export default function HomePage() {
 
@@ -14,7 +13,7 @@ export default function HomePage() {
     <>
         <div className= 'flex items-center justify-center min-h-screen '>
           <div className='max-w-4xl mt-[-250px] mx-auto p-8 bg-white rounded-4xl shadow-md'>
-            <img src={logo} alt='Logo' className='mt-6 mx-auto h-50 w-auto mb-10 logo-modify' />
+            <img src='./logoowen.svg' alt='Logo' className='mt-6 mx-auto h-50 w-auto mb-10 logo-modify' />
             <h1 className='text-center font-bold text-2xl'>Home Page</h1>
             <h2 className='text-center font-medium text-gray-400 text-xl mb-10'>User Management System</h2>
             <div className='flex justify-center space-x-4'>
